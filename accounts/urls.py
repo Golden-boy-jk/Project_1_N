d6d7b6d706334036977fb50d53a8c076ea1caca3
Evgeniy <aeon.kuzin@gmail.com>
2025-02-23 17:44:41 +0300

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import become_author, profile, activate_account
-
-
-urlpatterns = [
-    path('become_author/', become_author, name='become_author'),
-    path('profile/', profile, name='profile'),
-    path("activate/<str:signed_value>/", activate_account, name="activate_account"),
-]
