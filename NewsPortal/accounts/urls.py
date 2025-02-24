23b8c3c79732765311e665b0f8cb4431541bd8ee
Evgeniy <aeon.kuzin@gmail.com>
2025-02-24 20:07:06 +0300

    Delete NewsPortal directory

--- NewsPortal/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import become_author, profile
-
-urlpatterns = [
-    path('become_author/', become_author, name='become_author'),
-    path('profile/', profile, name='profile'),
-]
--- NewsPortal/news/news/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.apps import apps
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = apps.get_model('news', 'Post')
-        fields = ['title', 'text', 'author']
