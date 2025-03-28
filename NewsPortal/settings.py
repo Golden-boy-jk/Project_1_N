b00a3c267e0ba29902195ce9377f5d7f8548777d
Evgeniy <aeon.kuzin@gmail.com>
2025-03-28 23:27:16 +0300

    Add files via upload

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -1,12 +1,140 @@
 import os.path
 from pathlib import Path
 from celery.schedules import crontab
+from django.utils.log import DEFAULT_LOGGING
+import sys
 
+
+sys.stdout.reconfigure(encoding='utf-8')
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2za(ynv-&og)n8!%vk2e=u3=5-(k!6f*ip%0i%=m_1@2k37$4#'
 
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'console': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{',
+        },
+        'console_warning': {
+            'format': '{asctime} {levelname} {pathname} {message}',
+            'style': '{',
+        },
+        'console_error': {
+            'format': '{asctime} {levelname} {pathname} {message}',
+            'style': '{',
+        },
+        'file_general': {
+            'format': '{asctime} {levelname} {module} {message}',
+            'style': '{',
+        },
+        'file_error': {
+            'format': '{asctime} {levelname} {pathname} {message}',
+            'style': '{',
+        },
+        'file_security': {
+            'format': '{asctime} {levelname} {module} {message}',
+            'style': '{',
+        },
+        'mail_admins': {
+            'format': '{asctime} {levelname} {pathname} {message}',
+            'style': '{',
+        },
+    },
+
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+            'stream': sys.stdout,  # Указываем стандартный вывод
+        },
+        'console_warning': {
+            'level': 'WARNING',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_warning',
+        },
+        'console_error': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'console_error',
+        },
+        'file_general': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'file_general',
+        },
+        'file_error': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'errors.log',
+            'formatter': 'file_error',
+        },
+        'file_security': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'security.log',
+            'formatter': 'file_security',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'mail_admins',
+            'include_html': False,
+        },
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'console_warning', 'console_error', 'file_general'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['file_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['file_error', 'mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.template': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['file_error'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['file_security'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -153,9 +281,9 @@ EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'почта'
-EMAIL_HOST_PASSWORD = 'пароль от приложения'  # пароль от почты
-DEFAULT_FROM_EMAIL = 'почта'
+EMAIL_HOST_USER = 'stormus2006@yandex.ru'
+EMAIL_HOST_PASSWORD = 'vzuiajwqmcatihfk'  # пароль от почты
+DEFAULT_FROM_EMAIL = 'stormus2006@yandex.ru'
 SERVER_EMAIL = EMAIL_HOST_USER
 SITE_URL = 'http://127.0.0.1:8000'
 ABSOLUTE_URL_OVERRIDES = {
@@ -164,8 +292,8 @@ ABSOLUTE_URL_OVERRIDES = {
 ADMINS = [('Admin', 'game.kuzin@gmail.com'),]
 # MANAGERS = [('Manager', 'почто менеджера')]
 
-CELERY_BROKER_URL = 'redis://:ваш пароль-11985.c135.eu-central-1-1.ec2.redns.redis-cloud.com:11985'
-CELERY_RESULT_BACKEND = 'redis://:ваш пароль-11985.c135.eu-central-1-1.ec2.redns.redis-cloud.com:11985'
+CELERY_BROKER_URL = 'redis://:PsxDWUCVPMUbll6qyf40HoZ6KUFS5rKi@redis-11985.c135.eu-central-1-1.ec2.redns.redis-cloud.com:11985'
+CELERY_RESULT_BACKEND = 'redis://:PsxDWUCVPMUbll6qyf40HoZ6KUFS5rKi@redis-11985.c135.eu-central-1-1.ec2.redns.redis-cloud.com:11985'
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
@@ -184,3 +312,10 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
+CACHES = {
+    'default': {
+        'TIMEOUT': 300, # добавляем стандартное время ожидания в минуту (по умолчанию это 5 минут — 300 секунд)
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'), # Указываем, куда будем сохранять кэшируемые файлы! Не забываем создать папку cache_files внутри папки с manage.py!
+    }
+}
